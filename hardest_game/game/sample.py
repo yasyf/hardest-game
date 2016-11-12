f31512cd239533bbdffe7a1a2e2e5aa2126fdd45
Yasyf Mohamedali <yasyfm@gmail.com>
2016-11-12 06:40:17 -0500

    Convert to greyscale.

--- hardest_game/game/sample.py
+++ hardest_game/game/sample.py
@@ -1,6 +1,7 @@
 from pickleable import Pickleable
 import matplotlib.pyplot as plt
 from scipy.misc import imresize
+import numpy as np
 
 LARGE_NUMBER = 1000
 BLACK = 0
@@ -9,6 +10,7 @@ BACKGROUND = [180, 181, 254]
 BOARDER = [124, 124, 175]
 BLUE_TILE = [230, 230, 255]
 WHITE_TILE = [247, 247, 255]
+GREYSCALE = [0.299, 0.587, 0.114]
 
 class Sample(Pickleable):
   def __init__(self, state, image):
@@ -17,7 +19,7 @@ class Sample(Pickleable):
 
   def show(self):
     # plt.imshow(self.image)
-    plt.imshow(self.preprocess(self.image))
+    plt.imshow(self.preprocess(self.image), cmap = plt.get_cmap('gray'))
     plt.show()
 
   def reward(self):
@@ -30,6 +32,7 @@ class Sample(Pickleable):
     image[image == BOARDER] = BLACK
     image[image == BLUE_TILE] = WHITE
     image[image == WHITE_TILE] = WHITE
+    image = np.dot(image, GREYSCALE)
     image = imresize(image, 0.5)
     return image
 
