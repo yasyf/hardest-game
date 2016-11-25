afda1358fca1dec99502555cd4dd32dc751441d4
Yasyf Mohamedali <yasyfm@gmail.com>
2016-11-24 22:24:20 -0500

    Randomize default level.

--- hardest_game/toy_game/level.py
+++ hardest_game/toy_game/level.py
@@ -1,8 +1,17 @@
+import random
+from state import ToyGameState
+
 class Level(object):
+  RANDOM_DEFAULT = True
+  DEFAULT_STATIC_LEVEL = (2, 2)
+
   def __init__(self, enemy_loc, enemy_phase):
     self.enemy_loc = int(enemy_loc)
     self.enemy_phase = int(enemy_phase)
 
   @classmethod
   def default(cls):
-    return cls(2, 2)
+    if cls.RANDOM_DEFAULT:
+      return cls(random.randint(1, ToyGameState.MAX_X), 2)
+    else:
+      return cls(*cls.DEFAULT_STATIC_LEVEL)
