ca6c97fc3da4fce60f17ba8028f501c212e817cd
Yasyf Mohamedali <yasyfm@gmail.com>
2016-12-11 19:31:46 -0500

    Train on ToyGame2DHardSimulator.

--- hardest_game/learn/train/__main__.py
+++ hardest_game/learn/train/__main__.py
@@ -2,6 +2,7 @@ from deepq import DeepQTrainer
 from ...hardest_game.simulator import HardestGameSimulator
 from ...toy_game.simulator import ToyGameSimulator
 from ...toy_game_2d.simulator import ToyGame2DSimulator
+from ...toy_game_2d_hard.simulator import ToyGame2DHardSimulator
 
 def run_main(klass, **kwargs):
   trainer = DeepQTrainer(klass, **kwargs)
@@ -10,4 +11,5 @@ def run_main(klass, **kwargs):
 if __name__ == '__main__':
   # run_main(HardestGameSimulator)
   # run_main(ToyGameSimulator, num_steps=1e2)
-  run_main(ToyGame2DSimulator, num_steps=1e2)
+  # run_main(ToyGame2DSimulator, num_steps=1e2)
+  run_main(ToyGame2DHardSimulator, num_steps=1e2)
